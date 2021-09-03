4cc4d74e54e364c2c78eefb9448b495e530576e8
MrRedPanda69 <jorgeloredohdz@gmail.com>
2021-09-03 15:57:55 -0500

    made lots of things

--- src/app/heroes/pages/search/search.component.ts
+++ src/app/heroes/pages/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Hero } from '../../interfaces/heroes.interface';
+
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-search',
@@ -8,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  term: string = '';
+  heroes: Hero[] = [];
+
+  selectedHero: Hero | undefined;
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+  }
+
+  searching() {
+    this.heroesService.getSuggestions(this.term.trim())
+      .subscribe(heroesParam => this.heroes = heroesParam);
+  }
+
+  opSelected(event: MatAutocompleteSelectedEvent) {
+
+    if(!event.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero = event.option.value;
+    this.term = hero.superhero;
+
+    this.heroesService.getHeroById(hero.id)
+      .subscribe(hero => this.selectedHero = hero)
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,19 +3,26 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Hero } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseURL;
+
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>('http://localhost:3000/heroes');
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroById(id: string): Observable<Hero> {
-    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`); 
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`); 
+  }
+
+  getSuggestions(term: string): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${term}&_limit=6`);
   }
 }
