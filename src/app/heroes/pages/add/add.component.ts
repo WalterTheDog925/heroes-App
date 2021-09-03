5210b5044dc6e83df5895d1d4f01020dbe3b163f
MrRedPanda69 <jorgeloredohdz@gmail.com>
2021-09-03 18:29:10 -0500

    made lots of things

--- src/app/heroes/pages/add/add.component.ts
+++ src/app/heroes/pages/add/add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Hero, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-add',
@@ -8,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  hero: Hero = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: ''
+  }
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroesService.getHeroById(id))
+      )
+      .subscribe(hero => this.hero = hero)
+  }
+
+  save() {
+    if(this.hero.superhero.trim().length === 0) return;
+    
+    if(this.hero.id) {
+      this.heroesService.updateHero(this.hero)
+      .subscribe(hero => console.log('Updating', hero))
+    } else {
+
+    }
+    this.heroesService.addhero(this.hero)
+      .subscribe(resp => {
+        console.log('Respuesta', resp)
+      })
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -25,4 +25,12 @@ export class HeroesService {
   getSuggestions(term: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${term}&_limit=6`);
   }
+
+  addhero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero)
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    return this.http.put<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero)
+  }
 }
